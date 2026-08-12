b9c86c1831dd4e197996f7880252bf22e2c8c6ef
cllorey <cllorey@gmail.com>
2026-08-12 10:09:44 -0700

    initialize_reef.R

---
+++ initialize_reef.R
@@ -0,0 +1,22 @@
+set.seed(32)
+coral_matrix <- matrix(0, nrow = 5, ncol = 5)
+coral_matrix
+random <- sample(1:25, size = 8, replace = FALSE)
+random
+
+1:25
+
+# Rows
+1:25 %% 5
+(1:25 - 1) %% 5
+(1:25 - 1) %% 5 + 1
+
+# Columns
+1:25 %/% 5
+(1:25 - 1) %/% 5 + 1
+
+cell_label <- 6
+cell_row <- (6-1) %/% 5 + 1
+cell_col <- (6-1) %% 5 + 1
+coral_matrix[cell_row, cell_col] <- 1
+coral_matrix
